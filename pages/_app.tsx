eb52ac7f058c244f5de7853ad58c3db8f0537424
Halley Rios <hrios@symplicity.com>
2021-01-19 19:48:43 -0300

    feat: Added custom app

---
+++ pages/_app.tsx
@@ -0,0 +1,9 @@
+import type { AppProps } from 'next/app';
+
+const CustomApp = ({ Component, pageProps }: AppProps) => {
+    return (
+        <Component { ...pageProps } />
+    );
+}
+
+export default CustomApp;
