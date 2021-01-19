686ac71f1d11ab9e840d06a16e94fd7c8b97d5e5
Halley Rios <hrios@symplicity.com>
2021-01-19 19:40:59 -0300

    feat: Added TypeScript support and converted the index file.

--- pages/index.js
+++
@@ -1,5 +0,0 @@
-function Index() {
-    return <h2>Hello World!</h2>
-}
-
-export default Index;
---
+++ pages/index.tsx
@@ -0,0 +1,7 @@
+const Index = () => {
+    return (
+        <h1>Hello World with TypeScript!</h1>
+    );
+};
+
+export default Index;
